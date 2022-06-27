c39ffce4efee5fd0cde07f1efc8ed861aadd7568
Averageasd <nhuynh9497@gmail.com>
2022-06-27 12:23:58 -0400

    add features that allow user to start/stop drawing the grids. grids change color on hover

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 const gridContainer = document.querySelector('.grid-container');
 let boardSize = 16;
 let gridSize = gridContainer.offsetWidth/boardSize-2;
+let isDrawing = false;
+console.log(gridContainer.offsetWidth);
 console.log(gridSize);
 
 function setUpBoard(){
-    for (let i = 0; i<boardSize;i++){
+    for (let i = 0;i<boardSize;i++){
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         for (let j = 0;j<boardSize;j++){
             const cell = document.createElement('div');
-            cell.style.cssText = `width:${gridSize}px;height:${gridSize}px;border:1px black solid;background:gray`;
-            cell.addEventListener('click',()=>{
-                cell.style.background='blue';
-            });
+            cell.style.cssText = `width:${gridSize}px;height:${gridSize}px;border:1px black solid;background:gray;`;
             gridRow.appendChild(cell);
         }
         gridContainer.appendChild(gridRow);
+        
+    }
+}
+
+function addHoverCellFeature(){
+    for (let i = 0; i<boardSize;i++){
+        const cellRowContainer = gridContainer.children[i];
+        const cells = cellRowContainer.children;
+        console.log(cells);
+        for (let j = 0; j<cells.length;j++){
+            cells[j].addEventListener('mouseover',()=>{
+                if (isDrawing){
+                    cells[j].style.background = 'red';
+                }
+                
+            });
+            cells[j].addEventListener('click',()=>{
+                cells[j].style.background = 'red';
+                if (isDrawing){
+                    isDrawing = false;
+                }
+                else{
+                    isDrawing = true;
+                }
+            });
+        }
     }
 }
 
 
+
 setUpBoard();
+addHoverCellFeature();
+
